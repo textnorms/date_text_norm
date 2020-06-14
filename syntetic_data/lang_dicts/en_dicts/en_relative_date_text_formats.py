9b9383df2ca8e79f63b538ce48ce4303ef51c40a
Gustavo <gustavoplensack@gmail.com>
2020-06-14 10:19:49 -0300

    Introducing english relative date texts
    This commit implements the relative dates text formats for synthetic dates dataset.

--- syntetic_data/lang_dicts/en_dicts/en_relative_date_text_formats.py
+++ syntetic_data/lang_dicts/en_dicts/en_relative_date_text_formats.py
@@ -6,7 +6,7 @@ from num2words import num2words
 
 def format1(sample):
     '''
-        converts -7d to 7 days ago
+        Implements pair -7d and 7 days ago
     '''
     sample_text = sample
 
@@ -17,7 +17,7 @@ def format1(sample):
 
 def format2(sample):
     '''
-        Converts 7d to seven days ago
+        Implements pair -7d and seven days ago
     '''
     sample_text = num2words(sample,lang='en')
 
@@ -29,7 +29,7 @@ def format2(sample):
 
 def format3(sample):
     '''
-        converts -7m to 7 months ago
+        Implements pair -7m and 7 months ago
     '''
     sample_text = sample
 
@@ -40,7 +40,7 @@ def format3(sample):
 
 def format4(sample):
     '''
-        Converts -7m to seven months ago
+        Implements pair -7m and seven months ago
     '''
     sample_text = num2words(sample,lang='en')
 
@@ -52,7 +52,7 @@ def format4(sample):
 
 def format5(sample):
     '''
-        converts -7y to 7 years ago
+        Implements pair -7y and 7 years ago
     '''
     sample_text = sample
 
@@ -63,41 +63,170 @@ def format5(sample):
 
 def format6(sample):
     '''
-        Converts -7y to seven days ago
+        Implements pair -7y and seven years ago
     '''
     sample_text = num2words(sample,lang='en')
-
-    if sample > 1:
-        return f'{sample_text} days ago',f'-{sample}y'
-    else:
-        return f'{sample_text} day ago',f'-{sample}y'
-
-def format5(sample):
-    '''
-        converts -7y to 7 years ago
-    '''
-    sample_text = sample
 
     if sample > 1:
         return f'{sample_text} years ago',f'-{sample}y'
     else:
         return f'{sample_text} year ago',f'-{sample}y'
 
-def format6(sample):
+def format7(sample):
     '''
-        Converts -7y to seven days ago
+        Implements pair 7d and for 7 days
+    '''
+    sample_text = sample
+
+    if sample > 1:
+        return f'for {sample_text} days',f'{sample}d'
+    else:
+        return f'for {sample_text} day',f'{sample}d'
+
+def format8(sample):
+    '''
+        Implements pair 7d and for seven days
     '''
     sample_text = num2words(sample,lang='en')
 
     if sample > 1:
-        return f'{sample_text} days ago',f'-{sample}y'
+        return f'for {sample_text} days',f'{sample}d'
     else:
-        return f'{sample_text} day ago',f'-{sample}y'
+        return f'for {sample_text} day',f'{sample}d'
 
+def format9(sample):
+    '''
+        Implements pair 7m and for 7 months
+    '''
+
+    sample_text  = sample
+
+    if sample > 1:
+        return f'for {sample_text} months',f'{sample}m'
+    else:
+        return f'for {sample_text} month',f'{sample}m'
+
+def format10(sample):
+    '''
+        Implements pair 7m and for seven months
+    '''
+    sample_text = num2words(sample,lang='en')
+
+    if sample > 1:
+        return f'for {sample_text} months',f'{sample}m'
+    else:
+        return f'for {sample_text} month',f'{sample}m'
+
+def format11(sample):
+    '''
+        Implements pair 7y and for 7 years
+    '''
+
+    sample_text  = sample
+
+    if sample > 1:
+        return f'for {sample_text} years',f'{sample}y'
+    else:
+        return f'for {sample_text} year',f'{sample}y'
+
+def format12(sample):
+    '''
+        Implements pair 7y and for seven years
+    '''
+    sample_text = num2words(sample,lang='en')
+
+    if sample > 1:
+        return f'for {sample_text} years',f'{sample}y'
+    else:
+        return f'for {sample_text} year',f'{sample}y'
+
+
+def format13(sample):
+    '''
+        Implements pair 7d and during 7 days
+    '''
+
+    sample_text  = sample
+
+    if sample > 1:
+        return f'during {sample_text} days',f'{sample}d'
+    else:
+        return f'during {sample_text} day',f'{sample}d'
+
+def format14(sample):
+    '''
+        Implements pair 7d and during seven days
+    '''
+    sample_text = num2words(sample,lang='en')
+
+    if sample > 1:
+        return f'during {sample_text} days',f'{sample}d'
+    else:
+        return f'during {sample_text} day',f'{sample}d'
+
+def format15(sample):
+    '''
+        Implements pair 7m and during 7 months
+    '''
+
+    sample_text  = sample
+
+    if sample > 1:
+        return f'during {sample_text} months',f'{sample}m'
+    else:
+        return f'during {sample_text} month',f'{sample}m'
+
+def format16(sample):
+    '''
+        Implements pair 7m and during seven months
+    '''
+    sample_text = num2words(sample,lang='en')
+
+    if sample > 1:
+        return f'during {sample_text} months',f'{sample}m'
+    else:
+        return f'during {sample_text} month',f'{sample}m'
+
+def format17(sample):
+    '''
+        Implements pair 7y and during 7 years
+    '''
+
+    sample_text  = sample
+
+    if sample > 1:
+        return f'during {sample_text} years',f'{sample}y'
+    else:
+        return f'during {sample_text} year',f'{sample}y'
+
+def format18(sample):
+    '''
+        Implements pair 7y and during seven years
+    '''
+    sample_text = num2words(sample,lang='en')
+
+    if sample > 1:
+        return f'during {sample_text} years',f'{sample}y'
+    else:
+        return f'during {sample_text} year',f'{sample}y'
 
 relative_date_formats_dict = {
     '1':format1,
     '2':format2,
     '3':format3,
-    '4':format4
+    '4':format4,
+    '5':format5,
+    '6':format6,
+    '7':format7,
+    '8':format8,
+    '9':format9,
+    '10':format10,
+    '11':format11,
+    '12':format12,
+    '13':format13,
+    '14':format14,
+    '15':format15,
+    '16':format16,
+    '17':format17,
+    '18':format18
 }
